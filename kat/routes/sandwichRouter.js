64d2ba104e5ae33412df956d9550a53217ce515c
Katherine Beame <katherine.beame@gmail.com>
2016-04-21 21:21:04 -0700

    added routers for sandwiches

--- kat/routes/sandwichRouter.js
+++ kat/routes/sandwichRouter.js
@@ -14,3 +14,34 @@ sandwichRouter.post('/sandwich', bodyParser, (req, res) => {
     res.status(200).json(data);
   });
 });
+
+sandwichRouter.get('/sandwich', (req, res) => {
+  Sandwich.find({}, (err, data) => {
+    if(err) {
+      console.log(err);
+      return res.status(500).json({ msg: 'Server Error' });
+    }
+    res.status(200).json(data);
+  });
+});
+
+sandwichRouter.put('/sandwich/:id', bodyParser, (req, res) => {
+  var sandwichData = req.body;
+  delete sandwichData._id;
+  Sandwich.update({ _id: req.params.id }, sandwichData, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json(data);
+    }
+    res.status(200).json({ msg: 'Updated a sandwich entry with put' });
+  });
+});
+
+sandwichRouter.delete('/sandwich/:id', bodyParser, (req, res) => {
+  Sandwich.findOneAndRemove({ _id: req.params.id }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.status(200).json({ msg: 'Deleted a sandwich entry' });
+  });
+});
