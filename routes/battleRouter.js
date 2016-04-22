f04db7e46513d4b0b88ab179108fe2d8a800e511
BRANDON PARKER <Brandon@dkparker.com>
2016-04-22 10:57:40 -0700

    added a gulp file and tests now work for it

--- routes/battleRouter.js
+++ routes/battleRouter.js
@@ -1,8 +1,24 @@
 // const Router = require('express').Router;
 // const Jedi = require(__dirname + '/../models/jedi');
 // const Sith = require(__dirname + '/../models/sith');
-// const bodyParser = require('body-parser').json();
+// const mongoose = require('mongoose');
 // const errorHandler = require(__dirname + '/../lib/errorHandler');
 // var battleRouter = module.exports = Router();
 //
-// battleRouter
+// battleRouter.get('/battle', (req, res) =>
+//
+// });
+//
+//
+// versusRouter.get('/versus', (req, res) => {
+//   Jedi.find(null, (err, jediData) => {
+//     if (err) return errorHandler(err, res);
+//     Sith.find(null, (err, sithData) => {
+//       if (err) return errorHandler(err, res);
+//       res.status(200).json({
+//         sith: sithData.length,
+//         jedi: jediData.length
+//       });
+//     });
+//   });
+// });
