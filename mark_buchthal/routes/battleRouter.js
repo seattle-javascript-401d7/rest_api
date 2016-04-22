c9b744b796e539d6955176cc66db8de4b6362f75
Mark Buchthal <mark.buchthal@gmail.com>
2016-04-22 12:44:12 -0700

    eslint fix

--- mark_buchthal/routes/battleRouter.js
+++ mark_buchthal/routes/battleRouter.js
@@ -3,30 +3,28 @@ const Router = require('express').Router;
 const Hero = require(__dirname + '/../models/hero');
 const Villain = require(__dirname + '/../models/villain');
 const serverErrorHandler = require(__dirname + '/../lib/error_handler');
-const mongoose = require('mongoose');
 var battleRouter = module.exports = exports = Router();
-var heroLevel, villainLevel, villainPower, heroPower, heroName, villainName;
-var counter = 0;
+var heroLevel, villainLevel, villainPower, heroPower, heroName, villainName, randHero, randVillain;
 
 battleRouter.get('/battle', (req, res) => {
-  getHero((herodata) => {
-    var heroRandNum = Math.floor(Math.random() * randHero[0]['superPower'].length);
-    heroName = randHero[0]['name'];
-    heroPower = randHero[0]['superPower'][heroRandNum];
-    heroLevel = randHero[0]['powerLevel'];
-    getVillain((villaindata) => {
-      var villainRandNum = Math.floor(Math.random() * randHero[0]['superPower'].length);
-      villainName = randVillain[0]['name'];
-      villainPower = randVillain[0]['superPower'][villainRandNum];
-      villainLevel = randVillain[0]['powerLevel'];
+  getHero(() => {
+    var heroRandNum = Math.floor(Math.random() * randHero[0].superPower.length);
+    heroName = randHero[0].name;
+    heroPower = randHero[0].superPower[heroRandNum];
+    heroLevel = randHero[0].powerLevel;
+    getVillain(() => {
+      var villainRandNum = Math.floor(Math.random() * randHero[0].superPower.length);
+      villainName = randVillain[0].name;
+      villainPower = randVillain[0].superPower[villainRandNum];
+      villainLevel = randVillain[0].powerLevel;
       sendMessage(res);
     });
   });
 });
 
 function getHero(cb) {
-  Hero.aggregate({$sample:{size:1}}, (err, data) => {
-    if (err) return serverErrorHandler(err, res);
+  Hero.aggregate({ $sample: { size: 1 } }, (err, data) => {
+    if (err) return serverErrorHandler(err);
 
     randHero = data;
     cb(randHero);
@@ -34,8 +32,8 @@ function getHero(cb) {
 }
 
 function getVillain(cb) {
-  Villain.aggregate({$sample:{size:1}}, (err, data) => {
-    if (err) return serverErrorHandler(err, res);
+  Villain.aggregate( { $sample: { size: 1 } }, (err, data) => {
+    if (err) return serverErrorHandler(err);
 
     randVillain = data;
     cb(randVillain);
@@ -44,8 +42,10 @@ function getVillain(cb) {
 
 function sendMessage(res) {
   if (heroLevel > villainLevel) {
-    res.status(200).send('The ' + heroPower + ' of ' + heroName + ' has defeated ' + villainName + ' and their ' + villainPower +'!!');
+    res.status(200).send('The ' + heroPower + ' of ' + heroName +
+      ' has defeated ' + villainName + ' and their ' + villainPower + '!!');
   } else {
-    res.status(200).send('The ' + villainPower + ' of ' + villainName + ' has defeated ' + heroName + ' and their ' + heroPower +'!!');
+    res.status(200).send('The ' + villainPower + ' of ' + villainName +
+      ' has defeated ' + heroName + ' and their ' + heroPower + '!!');
   }
 }
