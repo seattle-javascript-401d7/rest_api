1cfe3c7b1680a185ecdb572403372df529d52670
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-22 10:33:44 -0700

    add error handler

--- carrie-short/routes/dinosaursrouter.js
+++ carrie-short/routes/dinosaursrouter.js
@@ -2,26 +2,19 @@ const Router = require('express').Router;
 const Dinosaur = require(__dirname + '/../models/dinosaur');
 const bodyParser = require('body-parser').json();
 const dinosaursRouter = module.exports = new Router();
+const serverErrorHandler = require(__dirname + '/../lib/error_handler');
 
 dinosaursRouter.post('/dinosaurs', bodyParser, (req, res) => {
   var newDinosaur = new Dinosaur(req.body);
   newDinosaur.save((err, data) => {
-    if (err) {
-      return res.status(500).json({
-        msg: 'dude, your server errored'
-      });
-    }
+    if (err) return serverErrorHandler(err, res);
     res.status(200).json(data);
   });
 });
 
 dinosaursRouter.get('/dinosaurs', (req, res) => {
   Dinosaur.find({}, (err, data) => {
-    if (err) {
-      return res.status(500).json({
-        msg: 'dude, your server errored'
-      });
-    }
+    if (err) return serverErrorHandler(err, res);
     res.status(200).json(data);
   });
 });
@@ -30,22 +23,14 @@ dinosaursRouter.put('/dinosaurs/:id', bodyParser, (req, res) => {
   var dinosaurData = req.body;
   delete dinosaurData._id;
   Dinosaur.update({ _id: req.params.id }, dinosaurData, (err, data) => {
-    if (err) {
-      return res.status(500).json({
-        msg: 'dude, your server errored'
-      });
-    }
+    if (err) return serverErrorHandler(err, res);
     res.status(200).json(data);
   });
 });
 
 dinosaursRouter.delete('/dinosaurs/:id', (req, res) => {
   Dinosaur.remove({ _id: req.params.id }, (err) => {
-    if (err) {
-      return res.status(500).json({
-        msg: 'dude, your server errored'
-      });
-    }
+    if (err) return serverErrorHandler(err, res);
     res.status(200).json({
       msg: 'dinosaur has gone extinct'
     });
--- carrie-short/routes/versusrouter.js
+++ carrie-short/routes/versusrouter.js
@@ -1,27 +1,18 @@
 const Router = require('express').Router;
 const Politician = require(__dirname + '/../models/politician');
 const Dinosaur = require(__dirname + '/../models/dinosaur');
-const bodyParser = require('body-parser').json();
 const versusRouter = module.exports = new Router();
+const serverErrorHandler = require(__dirname + '/../lib/error_handler');
 
 versusRouter.get('/versus', (req, res) => {
   Politician.find({}, (err, politicianData) => {
-    if (err) {
-      return res.status(500).json({
-        msg: 'dude, your server errored'
-      });
-    }
+    if (err) return serverErrorHandler(err, res);
     Dinosaur.find({}, (err, dinosaurData) => {
-      if (err) {
-        return res.status(500).json({
-          msg: 'dude, your server errored'
-        });
-      }
+      if (err) return serverErrorHandler(err, res);
       res.status(200).json({
         politicians: politicianData.length,
         dinosaurs: dinosaurData.length
       });
     });
   });
-
 });
