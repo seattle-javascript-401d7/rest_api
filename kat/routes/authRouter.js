b800933571f4e7f36f88c0a91349ab3594b37bbc
Katherine Beame <katherine.beame@gmail.com>
2016-04-27 12:47:21 -0700

    have post and get of authentication set up

--- kat/routes/authRouter.js
+++ kat/routes/authRouter.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const User = require(__dirname + '/../models/user.js');
 const bodyParser = require('body-parser').json();
+const basicHTTP = require(__dirname + '/../lib/basic_http.js');
 
 var router = module.exports = exports = express.Router();
 
 router.post('/signup', bodyParser, (req, res) => {
   var password = req.body.password;
   req.body.password = null;
+  if (!password.length) return res.status(500).json({ msg: 'Please include a password' });
   var newUser = new User(req.body);
   newUser.generateHash(password);
   password = null;
   newUser.save((err, data) => {
-    if (err) return res.ststaus(500).json({ msg: 'Invalid username'});
-    res.json({ msg: 'User Successfully Created!'});
+    if (err) return res.ststaus(500).json({ msg: 'Invalid username' });
+    res.json({ msg: 'User Successfully Created!' });
+  });
+});
+
+router.get('/signin', basicHTTP, (req, res) => {
+  User.findOne({ username: req.auth.username }, (err, user) => {
+    if (err) res.status(500).json({ msg: 'could not authenticate' });
+
+    if (!user) return res.status(500).json({ msg: 'could not authenticate' });
+
+    if (!user.compareHash(req.auth.password)) {
+      return res.status(500)
+      .json({ msg: 'could not authenticate' });
+    }
+
+    res.json({ msg: 'authenticate says yes' });
   });
 });
--- kat/server.js
+++ kat/server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 const petRouter = require(__dirname + '/routes/petRouter.js');
 const sandwichRouter = require(__dirname + '/routes/sandwichRouter.js');
 const warsRouter = require(__dirname + '/routes/combinedRouter.js');
+const authRouter = require(__dirname + '/routes/authRouter.js');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kat_db');
@@ -11,4 +12,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kat_db');
 app.use('/api', petRouter);
 app.use('/api', sandwichRouter);
 app.use('/api', warsRouter);
+app.use('/api', authRouter);
 app.listen(PORT, () => console.log('server alive at' + PORT));
