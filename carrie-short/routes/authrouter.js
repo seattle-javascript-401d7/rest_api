98c20f2088c53b2c628b6e9e35ffa0290ab9262f
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-27 09:37:27 -0700

    create function for parsing authorization header

---
+++ carrie-short/routes/authrouter.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const User = require(__dirname + '/../models/user');
+const bodyParser = require('body-parser').json();
+const basicHTTP = require(__dirname + '')
