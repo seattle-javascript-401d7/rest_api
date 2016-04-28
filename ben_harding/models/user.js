f67542e3972a79cfc70dd6b1ba0d3c78088d6356
bharding2 <b.harding@gmail.com>
2016-04-28 09:08:29 -0700

    updated user schema to generate hash and jwt

--- ben_harding/models/user.js
+++ ben_harding/models/user.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 var userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  findHash: { type: String, unique: true }
 });
 
 userSchema.methods.generateHash = function(password) {
@@ -12,6 +15,37 @@ userSchema.methods.generateHash = function(password) {
 
 userSchema.methods.compareHash = function(password) {
   return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.methods.generateFindHash = function(cb) {
+  var tries = 0;
+  var timeout;
+  var _generateFindHash = () => {
+    var hash = crypto.randomBytes(32);
+    this.findHash = hash.toString('hex');
+    this.save((err) => {
+      if (err) {
+        if (tries > 9) {
+          return cb(new Error('could not generate hash'));
+        }
+        return timeout = setTimeout(() => {
+          _generateFindHash();
+          tries++;
+        }, 1000);
+      }
+
+      if (timeout) clearTimeout(timeout);
+      cb(null, hash.toString('hex'));
+    });
+  };
+  _generateFindHash();
+};
+
+userSchema.methods.generateToken = function(cb) {
+  this.generateFindHash(function(err, hash) {
+    if (err) return cb(err);
+    cb(null, jwt.sign({ idd: hash }, process.env.APP_SECRET));
+  });
 };
 
 module.exports = exports = mongoose.model('User', userSchema);
