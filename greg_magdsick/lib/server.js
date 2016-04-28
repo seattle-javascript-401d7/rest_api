60529dfdcd3177b2a43b6e21eae7074dcff290f8
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-28 11:48:31 -0700

    refactored routes

--- greg_magdsick/lib/server.js
+++ greg_magdsick/lib/server.js
@@ -1,9 +1,13 @@
 const app = require('express')();
-const router = require(__dirname + '/../routes/router');
+const pedal = require(__dirname + '/../routes/pedal');
+const motor = require(__dirname + '/../routes/motor');
+const fast = require(__dirname + '/../routes/fast');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_LOC || 'mongodb://localhost/db');
 
-app.use('/api', router);
+app.use('/api', pedal);
+app.use('/api', motor);
+app.use('/api', fast);
 
 app.listen(PORT, () => process.stdout.write('server lisening on: ' + PORT + '\n'));
