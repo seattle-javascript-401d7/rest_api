a7e199110dfcf19d3f229dd705442170af84ac5c
KMSkelton <kmskelton@outlook.com>
2016-04-28 12:10:19 -0700

    authorization works: jwt included in db objects

--- skelton/lib/server.js
+++ skelton/lib/server.js
@@ -1,12 +1,28 @@
+if (!process.env.APP_SECRET) throw new Error('you need to set the APP_SECRET environment variable');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const shoesRouter = require(__dirname + '/../routes/shoes_router');
 const pantsRouter = require(__dirname + '/../routes/pants_router');
 const wardrobeRouter = require(__dirname + '/../routes/wardrobe_router');
+const authRouter = require(__dirname + '/../routes/auth_router');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/outfit_db');
 
+const fs = require('fs');
+
+function checkDirectory(directory) {
+  fs.stat(directory, (err) => {
+    if (err) {
+      fs.mkdir(directory);
+      return console.log('made directory');
+    }
+    console.log('dir exists');
+  });
+}
+checkDirectory('./data/');
+
+app.use('/', authRouter);
 app.use('/', wardrobeRouter);
 app.use('/api', shoesRouter);
 app.use('/api', pantsRouter);
