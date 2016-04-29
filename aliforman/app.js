38a33ff005515a0e544741ce781f56b2acb9fb59
AlegriaForman <alegriaforman@gmail.com>
2016-04-28 22:32:55 -0700

    add app.js

--- aliforman/app.js
+++ aliforman/app.js
@@ -1,23 +1,6 @@
-'use strict';
-
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/db');
+if (!process.env.APP_SECRET) throw new Error('You need to set the APP_SECRET environment variable');
 
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-
-const jawaRouter = require(__dirname + '/routes/jawaRoutes');
-const droidRouter = require(__dirname + '/routes/droidRoutes');
-const addressRouter = require(__dirname + '/routes/query');
-
-app.use('/api', jawaRouter);
-app.use('/api', droidRouter);
-app.use('/api', addressRouter);
-
-app.listen(port, () => {
-  console.log('Server listening on port ' + (port || 3000));
-});
+var app = require(__dirname + '/_app.js');
+app.listen(port, 'mongodb://localhost/db', () => console.log('server up on port:' + port));
