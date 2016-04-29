29a652c1bac363f3305ed7971c0f5f4e8a255ab4
AlegriaForman <alegriaforman@gmail.com>
2016-04-28 22:34:11 -0700

    revise serverErrHandler.js

--- aliforman/lib/serverErrHandler.js
+++ aliforman/lib/serverErrHandler.js
@@ -1,4 +1,4 @@
 module.exports = function(err, res) {
   console.log(err);
-  res.status(500).json({ msg: 'There is a problem with the server.' });
+  res.status(500).json({ msg: 'There is an internal server error.' });
 };
