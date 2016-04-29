fe7fc8ebd1a4c3dbb0a33384b0e3e2329fd23588
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-29 05:43:22 -0700

    first step

--- greg_magdsick/lib/server.js
+++ greg_magdsick/lib/server.js
@@ -1,16 +1,10 @@
-const app = require('express')();
-const pedal = require(__dirname + '/../routes/pedal');
-const motor = require(__dirname + '/../routes/motor');
-const fast = require(__dirname + '/../routes/fast');
-const mongoose = require('mongoose');
-const PORT = process.env.PORT || 5000;
+if (!process.env.APP_SECRET) {
+  throw new Error('APP_SECRET environment variable not set.');
+}
 
-mongoose.connect(process.env.MONGO_LOC || 'mongodb://localhost/db');
+const app = require(__dirname + '/_server');
 
-app.use('/api', pedal);
-app.use('/api', motor);
-app.use('/api', fast);
+var PORT = process.env.PORT || 5000;
+var mongo = process.env.MONGO_LOC || 'mongodb://localhost/db';
 
-module.exports = exports = app.listen(PORT, () => {
-  process.stdout.write('server lisening on: ' + PORT + '\n');
-});
+app.listen(PORT, mongo, () => process.stdout.write('Server up on port: ' + PORT + '\n'));
