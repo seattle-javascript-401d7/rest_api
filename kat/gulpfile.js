8e0c6fec974b31e992d8dff5226c0d3be9792060
Katherine Beame <katherine.beame@gmail.com>
2016-05-10 22:12:57 -0700

    added eslintrc to client-side and seperated gulp lint

--- kat/gulpfile.js
+++ kat/gulpfile.js
@@ -4,18 +4,19 @@ const eslint = require('gulp-eslint');
 
 var files = ['lib**/*.js', 'test/**/*.js', 'routes/**/*.js',
 'models/**/*.js', 'gulpfile.js', 'server.js'];
+var clientFiles = ['./client/app**/*.js'];
 
-
-gulp.task('lint', () => {
+gulp.task('lintServer', () => {
   return gulp.src(files)
-  .pipe(eslint({
-    envs: [
-      'mocha',
-      'es6'
-    ]
-  }))
+  .pipe(eslint('./.eslintrc'))
   .pipe(eslint.format());
 });
 
 
-gulp.task('default', ['lint']);
+gulp.task('lintClient', () => {
+  return gulp.src(clientFiles)
+  .pipe(eslint('./client/app/.eslintrc'))
+  .pipe(eslint.format());
+});
+
+gulp.task('default', ['lintServer', 'lintClient']);
