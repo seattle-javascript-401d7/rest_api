6a81553d517ffec12a345f74921d0be4bc4c878c
KMSkelton <kmskelton@outlook.com>
2016-05-11 23:14:07 -0700

    updated: package.json, gulp file. The pieces should be working

--- skelton/gulpfile.js
+++ skelton/gulpfile.js
@@ -1,10 +1,11 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
+const webpack = require('webpack-stream');
 
 var files = ['./lib/**/*.js', './models/**/*.js', './routes/**/*.js', 'gulpfile.js'];
 
-gulp.task('lint:test', () => {
+gulp.task('lint:test', ['lint:nontest'], () => {
   return gulp
   .src('./test/**/*test.js')
   .pipe(mocha({reporter: 'nyan'}))
@@ -17,4 +18,27 @@ gulp.task('lint:nontest', () => {
   .pipe(eslint())
   .pipe(eslint.format());
 });
-gulp.task('default', ['lint:test', 'lint:nontest']);
+
+gulp.task('webpack:dev', ['lint:test'], () => {
+  gulp.src('./app/js/entry.js')
+    .pipe(webpack({
+      devtool: 'source-map',
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('static:dev', ['webpack:dev'], () => {
+  gulp.src('/app/**/*.html')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('css:dev', ['webpack:dev'], () => {
+  gulp.src('/app/css/**/*.html')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('builder', ['webpack:dev', 'static:dev', 'css:dev'])
+gulp.task('default', ['lint:test', 'lint:nontest', 'builder']);
