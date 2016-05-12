32cdb96c5d0e2d287bcf2f32f7a984799f6555bb
KMSkelton <kmskelton@outlook.com>
2016-05-11 20:25:32 -0700

    oops, wrong repo

---
+++ skelton/app/js/entry.js
@@ -0,0 +1,13 @@
+const angular = require('angular');
+
+const crudNG = angular.module('crudNG', []);
+const baseUrl = 'http://localhost:3030';
+
+const handleError = require('../../lib/error_handler');
+
+crudNG.controller('ShoesController', ['$http', function($http) {
+  this.shoes = [];
+  this.getAll = () => {
+    $http.get(baseUrl + '')
+  }
+  }])
