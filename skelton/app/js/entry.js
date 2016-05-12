fcdfe3526c79cfb36666431fd1af7d107e8c8998
KMSkelton <kmskelton@outlook.com>
2016-05-11 21:39:31 -0700

    keeping it all in the correct dir

--- skelton/app/js/entry.js
+++ skelton/app/js/entry.js
@@ -6,8 +6,12 @@ const baseUrl = 'http://localhost:3030';
 const handleError = require('../../lib/error_handler');
 
 crudNG.controller('ShoesController', ['$http', function($http) {
-  this.shoes = [];
-  this.getAll = () => {
-    $http.get(baseUrl + '')
-  }
-  }])
+    this.shoes = [];
+    this.getAll = () => {
+        $http.get(baseUrl + '/api/shoes')
+            .then((res) => {
+                this.shoes = res.data;
+            }, handleError.bind(this));
+    };
+
+}])
