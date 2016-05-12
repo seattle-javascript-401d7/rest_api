59e70e4bfa0711f4710bb39773b1694ac4a51db6
KMSkelton <kmskelton@outlook.com>
2016-05-11 22:37:25 -0700

    Angular pieces appear to be in place

--- skelton/app/js/entry.js
+++ skelton/app/js/entry.js
@@ -6,12 +6,51 @@ const baseUrl = 'http://localhost:3030';
 const handleError = require('../../lib/error_handler');
 
 crudNG.controller('ShoesController', ['$http', function($http) {
-    this.shoes = [];
-    this.getAll = () => {
-        $http.get(baseUrl + '/api/shoes')
-            .then((res) => {
-                this.shoes = res.data;
-            }, handleError.bind(this));
-    };
+  this.shoes = [];
+  this.getAll = () => {
+    $http.get(baseUrl + '/api/shoes')
+      .then((res) => {
+        this.shoes = res.data;
+      }, handleError.bind(this));
+  };
 
-}])
+  this.createShoe = () => {
+    $http.post(baseUrl + '/api/shoes', this.newShoe)
+      .then((res) => {
+        this.shoes.push(res.data);
+        this.newShoe = null;
+      }, handleError.bind(this));
+  };
+
+  this.removeShoe = (shoe) => {
+    $http.delete(baseUrl + '/api/shoes/' + shoe._id)
+      .then(() => {
+        this.shoes.splice(this.shoes.indexOf(shoe), 1);
+      }, handleError.bind(this));
+  };
+}]);
+
+crudNG.controller('PantsController', ['$http', function($http) {
+  this.pants = [];
+  this.getAll = () => {
+    $http.get(baseUrl + '/api/pants')
+      .then((res) => {
+        this.pants = res.data;
+      }, handleError.bind(this));
+  };
+
+  this.createPant = () => {
+    $http.post(baseUrl + '/api/pants', this.newPant)
+      .then((res) => {
+        this.pants.push(res.data);
+        this.newPant = null;
+      }, handleError.bind(this));
+  };
+
+  this.removePant = (pant) => {
+    $http.delete(baseUrl + '/api/pants/' + pant._id)
+      .then(() => {
+        this.pants.splice(this.pants.indexOf(pant), 1);
+      }, handleError.bind(this));
+  };
+}]);
