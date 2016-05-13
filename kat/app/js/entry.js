a8df8962ce7d585c6029cdcb83101dde9af9d452
Katherine Beame <katherine.beame@gmail.com>
2016-05-12 21:29:38 -0700

    added sandwich client side routing

--- kat/app/js/entry.js
+++ kat/app/js/entry.js
@@ -36,3 +36,33 @@ practiceApp.controller('PetController', ['$http', function($http) {
       }, handleErrors.bind(this));
   };
 }]);
+
+
+practiceApp.controller('SandwichController', ['$http', function($http) {
+  this.sandwich = [];
+  this.getAll = () => {
+    $http.get(url + '/api/sandwich')
+    .then((res) => {
+      this.sandwich = res.data;
+    }, handleErrors.bind(this));
+  };
+  this.createSandwich = () => {
+    $http.post(url + '/api/sandwich', this.newSandwich)
+      .then((res) => {
+        this.sandwich.push(res.data);
+        this.newSandwich = null;
+      }, handleErrors.bind(this));
+  };
+  this.deleteSandwich = (sandwich) => {
+    $http.delete(url + '/api/sandwich/' + sandwich._id)
+      .then(() => {
+        this.sandwich.splice(this.sandwich.indexOf(sandwich), 1);
+      }, handleErrors.bind(this));
+  };
+  this.updateSandwich = (sandwich) => {
+    $http.put(url + '/api/sandwich/' + sandwich._id, sandwich)
+      .then(() => {
+        sandwich.editing = false;
+      }, handleErrors.bind(this));
+  };
+}]);
