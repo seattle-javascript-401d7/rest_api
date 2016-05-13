fce9b2d5e0eff5375a2054a34d617808ec6c1df5
Hollis Lau <hollislau@hotmail.com>
2016-05-13 13:02:37 -0700

    Add middleware to handle CORS errors

--- hollis_lau/_server.js
+++ hollis_lau/_server.js
@@ -5,6 +5,13 @@ const starTrekCharsRouter = require(__dirname + "/routes/star_trek_chars_router"
 const starWarsCharsRouter = require(__dirname + "/routes/star_wars_chars_router");
 const battleRouter = require(__dirname + "/routes/battle_router");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT");
+  next();
+});
+
 app.use("/api", starTrekCharsRouter);
 app.use("/api", starWarsCharsRouter);
 app.use("/api", battleRouter);
