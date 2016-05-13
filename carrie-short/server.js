138a0fc08fcc7861365f4c923d6b0ebb155d1d4e
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-12 20:45:46 -0700

    update function expression to arrow function

--- carrie-short/server.js
+++ carrie-short/server.js
@@ -11,7 +11,7 @@ const authRouter = require(__dirname + '/routes/authrouter');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/political_dinos_db');
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
