bbf75f6e2d27e7aa54e20dfa225b078ba672b3ec
Katherine Beame <katherine.beame@gmail.com>
2016-05-12 20:58:23 -0700

    have CRUD working for Pets

--- kat/app/js/entry.js
+++ kat/app/js/entry.js
@@ -1,3 +1,38 @@
 const angular = require('angular');
 
 const practiceApp = angular.module('practiceApp', []);
+const url = 'http://localhost:5555';
+
+var handleErrors = function(error) {
+  console.log(error);
+  this.errors = (this.errors || []).push(error);
+};
+
+practiceApp.controller('PetController', ['$http', function($http) {
+  this.pet = [];
+  this.getAll = () => {
+    $http.get(url + '/api/pet')
+    .then((res) => {
+      this.pet = res.data;
+    }, handleErrors.bind(this));
+  };
+  this.createPet = () => {
+    $http.post(url + '/api/pet', this.newPet)
+      .then((res) => {
+        this.pet.push(res.data);
+        this.newPet = null;
+      }, handleErrors.bind(this));
+  };
+  this.deletePet = (pet) => {
+    $http.delete(url + '/api/pet/' + pet._id)
+      .then(() => {
+        this.pet.splice(this.pet.indexOf(pet), 1);
+      }, handleErrors.bind(this));
+  };
+  this.updatePet = (pet) => {
+    $http.put(url + '/api/pet/' + pet._id, pet)
+      .then(() => {
+        pet.editing = false;
+      }, handleErrors.bind(this));
+  };
+}]);
--- kat/server.js
+++ kat/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5555;
 const petRouter = require(__dirname + '/routes/petRouter.js');
 const sandwichRouter = require(__dirname + '/routes/sandwichRouter.js');
 const warsRouter = require(__dirname + '/routes/combinedRouter.js');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kat_db');
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  next();
+});
+
 
 app.use('/api', petRouter);
 app.use('/api', sandwichRouter);
