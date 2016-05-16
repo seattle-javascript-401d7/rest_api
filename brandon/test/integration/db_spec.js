66dd800f690d6a95fc5bbd31352466a9fc4fca36
BRANDON PARKER <Brandon@dkparker.com>
2016-05-16 09:13:14 -0700

    tests are working for jedi

--- brandon/test/integration/db_spec.js
+++ brandon/test/integration/db_spec.js
@@ -7,7 +7,7 @@ describe('a simple test to pass', () => {
     element(by.model('jediCtrl.newJedi.lightsaberColor')).sendKeys('WHO CARES');
     element(by.model('jediCtrl.newJedi.catchphrase')).sendKeys('WOOOOO');
     element(by.model('jediCtrl.newJedi.handCount')).sendKeys('2');
-    element(by.css('#jediList li:last-child .btn-create-jedi')).click();
+    element(by.css('.create-jedi')).click();
     element(by.css('#jediList li:last-child p')).getText((text) => {
       expect(text).toEqual('testman is a badass who uses a stick thing. Their lightsaber is ' +
        'WHO CARES and their catchphrase is WOOOOO with 2 hands.');
@@ -24,14 +24,14 @@ describe('a simple test to pass', () => {
 
   it('should not update from cancel button', () => {
     browser.get('http://localhost:5000');
-    element(by.css('#jediList li:last-child .btn-edit-jedi')).click();
+    element(by.css('#jediList li:last-child .edit-jedi')).click();
     element(by.model('jedi.name')).clear().sendKeys('a new jedi');
     element(by.model('jedi.ranking')).clear().sendKeys('noob');
     element(by.model('jedi.weaponPreference')).clear().sendKeys('computer');
     element(by.model('jedi.lightsaberColor')).clear().sendKeys('black and white');
     element(by.model('jedi.catchphrase')).clear().sendKeys('new phrase');
     element(by.model('jedi.handCount')).clear().sendKeys('1');
-    element(by.css('#jediList li:last-child .btn-cancel-jedi')).click();
+    element(by.css('#jediList li:last-child .cancel-jedi')).click();
     element(by.css('#jediList li:last-child p')).getText().then((text) => {
       expect(text).toEqual('testman is a badass who uses a stick thing. Their lightsaber is ' +
        'WHO CARES and their catchphrase is WOOOOO with 2 hands.');
@@ -40,14 +40,14 @@ describe('a simple test to pass', () => {
 
   it('should update the last jedi in the list', () => {
     browser.get('http://localhost:5000');
-    element(by.css('#jediList li:last-child .btn-edit-jedi')).click();
+    element(by.css('#jediList li:last-child .edit-jedi')).click();
     element(by.model('jedi.name')).clear().sendKeys('a new jedi');
     element(by.model('jedi.ranking')).clear().sendKeys('noob');
     element(by.model('jedi.weaponPreference')).clear().sendKeys('computer');
     element(by.model('jedi.lightsaberColor')).clear().sendKeys('black and white');
     element(by.model('jedi.catchphrase')).clear().sendKeys('new phrase');
     element(by.model('jedi.handCount')).clear().sendKeys('1');
-    element(by.css('#jediList li:last-child .btn-update-jedi')).click();
+    element(by.css('#jediList li:last-child .update-jedi')).click();
     element(by.css('#jediList li:last-child p')).getText().then((text) => {
       expect(text).toEqual('a new jedi is a noob who uses a computer. Their lightsaber is ' +
       'black and white and their catchphrase is new phrase with 1 hands.');
@@ -56,7 +56,7 @@ describe('a simple test to pass', () => {
 
   it('should delete the test jedi', () => {
     browser.get('http://localhost:5000');
-    element(by.css('#jediList li:last-child .btn-remove-jedi')).click();
+    element(by.css('#jediList li:last-child .remove-jedi')).click();
     element(by.css('#jediList li:last-child p')).getText().then((text) => {
       expect(text).not.toEqual('a new jedi is a noob who uses a computer. Their lightsaber is ' +
       'black and white and their catchphrase is new phrase with 1 hands.');
