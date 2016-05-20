bb2e629fb658b6b93629f3181ee51e5cb8b7dcc4
Mark Buchthal <mark.buchthal@gmail.com>
2016-05-20 09:35:21 -0700

    update server

--- mark_buchthal/server.js
+++ mark_buchthal/server.js
@@ -9,6 +9,13 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/hero_db');
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  next();
+});
+
 app.use('/api', heroRouter);
 app.use('/api', villainRouter);
 app.use('/api', battleRouter);
