d232a6e47008acc4fde8b6c89bb014b176b66204
THXBurke <rachelwheels@gmail.com>
2016-05-22 16:45:00 -0700

    gulpfile and tidy up messy file structure

--- RachelBurke/gulpFile.js
+++ RachelBurke/gulpFile.js
@@ -1,20 +1,55 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
-const mocha = require('gulp-mocha');
+const webpack = require('webpack-stream');
 
-var files = ['/nodecellar/**', '/models/**', '/routes/**', '/test/**', '/winecellar'];
-console.log(__dirname);
+var files = ['/nodecellar/**/*.js', '/models/**/*.js', '/routes/**/*.js', 'server.js'];
+var clientFiles = ['app/**/*.js'];
+var testFiles = ['test/unit/wine_controllertest.js',
+'test/unit/cheese_controllertest.js', 'test/unit/*.js'];
 
-gulp.task('eslint', () => {
+gulp.task('lintServer', () => {
   return gulp.src(files)
-  .pipe(eslint())
-  .pipe(eslint( { reporter: 'nyan' }));
+  .pipe(eslint('./.eslintrc'))
+  .pipe(eslint.format( { reporter: 'nyan' } ));
 });
 
-gulp.task('mocha', () => {
-  return gulp.src(files)
-  .pipe(mocha())
-  .pipe(mocha( { reporter: 'nyan' }));
+gulp.task('lintClient', () => {
+  return gulp.src(clientFiles)
+  .pipe(eslint('./app/.eslintrc'))
+  .pipe(eslint.format());
+});
+gulp.task('lintTest', () => {
+  return gulp.src(testFiles)
+  .pipe(eslint('./test/unit/.eslintrc'))
+  .pipe(eslint.format());
 });
 
-gulp.task('default', ['eslint', 'mocha']);
+gulp.task('webpack:dev', () => {
+  gulp.src('./app/js/entry.js')
+  .pipe(webpack({
+    output: {
+      filename: 'bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('./build'));
+});
+
+gulp.task('webpack:test', () => {
+  gulp.src('./test/unit/test_entry.js')
+  .pipe(webpack({
+    output: {
+      filename: 'bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('./test'));
+});
+
+gulp.task('static', () => {
+  gulp.src('app/**/*.html')
+  .pipe(gulp.dest('./build'));
+  gulp.src('app/**/*.css')
+  .pipe(gulp.dest('./build'));
+});
+
+gulp.task('default', ['lintServer', 'lintClient', 'lintTest', 'webpack:dev', 'static']);
+gulp.task('build', ['webpack:dev', 'static', 'webpack:test']);
