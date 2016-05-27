8d383a39d5f7e09010b08358d1ef5b381da8b619
THXBurke <rachelwheels@gmail.com>
2016-05-27 09:31:01 -0700

    factory

--- RachelBurke/app/js/entry.js
+++ RachelBurke/app/js/entry.js
@@ -3,3 +3,18 @@ const practiceApp = angular.module('practiceApp', []);
 
 require('./wine')(practiceApp);
 require('./cheese')(practiceApp);
+
+practiceApp.factory('cfStore', ['$rootScope', function($rs) {
+  return {
+    count: 0,
+    addCount: function() {
+      this.count++;
+    }
+  };
+}]);
+
+practiceApp.controller('CountCtrl', ['cfStore', function(cfStore) {
+  this.counter = cfStore;
+  this.getCount = cfStore.getCount.bing(cfStore);
+  this.add = cfStore.addCount.bind(cfStore);
+}]);
