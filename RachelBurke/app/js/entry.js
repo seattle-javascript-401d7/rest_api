4a8c625f2701d6d2ed35ffb42fa559866548823e
THXBurke <rachelwheels@gmail.com>
2016-05-30 23:09:32 -0700

    start the service counter process

--- RachelBurke/app/js/entry.js
+++ RachelBurke/app/js/entry.js
@@ -3,3 +3,28 @@ const practiceApp = angular.module('practiceApp', []);
 
 require('./wine')(practiceApp);
 require('./cheese')(practiceApp);
+
+practiceApp.factory('storageService', [ function() {
+  return {
+    count: 0,
+    addCount: function() {
+      this.count++;
+    }
+  };
+}]);
+
+practiceApp.controller('WineController', ['storageService', function(storageService) {
+  this.service = storageService;
+  this.serviceAddCount = storageService.addCount.bind(storageService);
+  this.count = 0;
+  this.addCount = function() {
+    this.count++;
+  };
+}]);
+
+practiceApp.controller('CheeseController', [ function() {
+  this.count = 0;
+  this.addCount = function() {
+    this.count++;
+  };
+}]);
