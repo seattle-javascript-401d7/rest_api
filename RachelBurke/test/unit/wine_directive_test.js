09aefb52d9f372eab11ab3b748cf55fdc2e4cede
THXBurke <rachelwheels@gmail.com>
2016-06-01 15:51:34 -0700

    update tests

--- RachelBurke/test/unit/wine_directive_test.js
+++ RachelBurke/test/unit/wine_directive_test.js
@@ -1,6 +1,6 @@
 var angular = require('angular');
-var wineFormTemplate = require(../../app/templates/wine_directives/wine_form.html);
-var cheeseFormTemplate = require(../../app/templates/cheese_directives/cheese_form.html);
+var wineFormTemplate = require('../../app/templates/wine_directives/wine_form.html');
+var cheeseFormTemplate = require('../../app/templates/cheese_directives/cheese_form.html');
 require('angular-mocks');
 
 describe('wine form directive', function() {
@@ -9,20 +9,22 @@ describe('wine form directive', function() {
   var $httpBackend;
 
   beforeEach(angular.mock.module('practiveApp'));
-  $compile = _$compile_;
-  $scope = $rootScope.$new();
-  $httpBackend = _$httpBackend_;
-}));
 
-it('button in form should be customizable', function() {
-  $httpBackend.when('GET', '/templates/wine_directives/wine_form.html')
+  beforeEach(angular.mock.inject(function(_$compile_, $rootScope, _$httpBackend_) {
+    $compile = _$compile_;
+    $scope = $rootScope.$new();
+    $httpBackend = _$httpBackend_;
+  }));
+
+  it('button in form should be customizable', function() {
+    $httpBackend.when('GET', '/templates/wine_directives/wine_form.html')
   .respond(200, wineListTemplate);
-  $scope.wine = {
-    name: 'Merlot',
-    description: 'Dry',
-  };
-  var listElement = $compile('<section data-ng-controller = 'WineController as winecontrol'><wine-list-item data-wine='wine'></wine-list-item></section>')($scope);
-  $httpBackend.flush();
-  expect(listElement.html()).toContain('Merlot usually is not dry');
+    $scope.wine = {
+      name: 'Merlot',
+      description: 'Dry'
+    };
+    var listElement = $compile('<section data-ng-controller = "WineController as winecontrol"><wine-list-item data-wine="wine"></wine-list-item></section>')($scope);
+    $httpBackend.flush();
+    expect(listElement.html()).toContain('Merlot usually is not dry');
   });
 });
