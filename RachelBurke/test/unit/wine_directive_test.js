99ce986b362c4797fd6dc54c8280d9d486541eee
THXBurke <rachelwheels@gmail.com>
2016-06-01 16:26:07 -0700

    fixed linting errors

--- RachelBurke/test/unit/wine_directive_test.js
+++ RachelBurke/test/unit/wine_directive_test.js
@@ -1,6 +1,6 @@
 var angular = require('angular');
 var wineFormTemplate = require('../../app/templates/wine_directives/wine_form.html');
-var cheeseFormTemplate = require('../../app/templates/cheese_directives/cheese_form.html');
+var wineListTemplate = require('../../app/templates/wine_directives/wine_form.html');
 require('angular-mocks');
 
 describe('wine form directive', function() {
@@ -8,7 +8,7 @@ describe('wine form directive', function() {
   var $compile;
   var $httpBackend;
 
-  beforeEach(angular.mock.module('practiveApp'));
+  beforeEach(angular.mock.module('practiceApp'));
 
   beforeEach(angular.mock.inject(function(_$compile_, $rootScope, _$httpBackend_) {
     $compile = _$compile_;
@@ -17,12 +17,20 @@ describe('wine form directive', function() {
   }));
 
   it('button in form should be customizable', function() {
-    $httpBackend.when('GET', '/templates/wine_directives/wine_form.html')
-  .respond(200, wineListTemplate);
+    $httpBackend.when('GET', '/templates/wine_directives/wine_form.html').respond(200, wineFormTemplate);
+    var element = $compile('<section data-ng-controller="WineController as winecontrol"><wine-form data-button-text="Test Wine" data-wine=" {}"></wine-form></section>')($scope);
+    $httpBackend.flush();
+    $scope.$digest();
+    expect(element.html()).toContain('Test Wine');
+  });
+
+  it('should print a wine list item', function() {
+    $httpBackend.when('GET', '/templates/wine_directives/wine_list_item.js').respond(200, wineListTemplate);
     $scope.wine = {
       name: 'Merlot',
       description: 'Dry'
     };
+
     var listElement = $compile('<section data-ng-controller = "WineController as winecontrol"><wine-list-item data-wine="wine"></wine-list-item></section>')($scope);
     $httpBackend.flush();
     expect(listElement.html()).toContain('Merlot usually is not dry');
